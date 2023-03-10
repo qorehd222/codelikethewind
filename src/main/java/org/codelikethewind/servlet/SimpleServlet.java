940e45b6b59cb0cb87c065a08c6aa17600a91bbb
GyudongLee <rbehddl0717@google.com>
2023-03-10 10:40:07 +0900

    test5

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello BAEK's World 222222" + "</h1>");
+		out.println("<h1>" + "Hello BAEK's World 33333333333333" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
