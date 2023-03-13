1266c1f8115561e547fdddd86776fe84f0a49072
qorehd222 <qorehd02@gmail.com>
2023-03-13 15:11:35 +0900

    retest2

--- src/main/java/org/codelikethewind/servlet/SimpleServlet.java
+++ src/main/java/org/codelikethewind/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<body>");
-		out.println("<h1>" + "Hello BAEK's World 44444444444444" + "</h1>");
+		out.println("<h1>" + "Hello BAEK's World 5555555" + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
